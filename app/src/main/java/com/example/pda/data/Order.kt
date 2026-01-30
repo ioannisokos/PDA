1b6d23115451c1059923aa2ede37f0a6ba702e86
ioannisokos <92680731+ioannisokos@users.noreply.github.com>
2026-01-30 15:03:34 +0200

    Delete app/src/main/java/com/example/pda/data/Order.kt

--- app/src/main/java/com/example/pda/data/Order.kt
+++
@@ -1,8 +0,0 @@
-package com.example.pda.data
-
-data class Order(
-    val id: String = "",
-    val tableId: String = "",
-    val items: List<ProductItem> = listOf(),
-    val status: String = "pending"
-)
