720a1de747a14b82c304d0352bf674b6cae503a2
ioannisokos <92680731+ioannisokos@users.noreply.github.com>
2026-01-30 15:24:41 +0200

    Update TableEntity.kt

--- app/src/main/java/com/example/pda/data/TableEntity.kt
+++ app/src/main/java/com/example/pda/data/TableEntity.kt
@@ -1,6 +1,6 @@
 package com.example.pda.data
 
 data class TableEntity(
-    var id: String = "",       // Πρέπει να έχει default τιμή ""
-    var totalPrice: Double = 0.0 // Πρέπει να έχει default τιμή 0.0
+    var id: String = "",       // Must have ""
+    var totalPrice: Double = 0.0 //Must have value 0.0
 )
